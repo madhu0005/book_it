101b470d13303ea1b9b8f1fce187ef7dde3218df
madhu0005 <singhmadhusmita521@gmail.com>
2024-07-30 18:05:06 +0530

    2nd commit
    numOfBeds: 5

--- seeder/data.ts
+++ seeder/data.ts
@@ -150,7 +150,7 @@ export const rooms = [
       "Our largest room with a queen bed and foldable sofa. Comfortably fits 2-3 adults, four adults maximum. Decorated with Irish-themed colors and arts. It has a private bathroom with a shower, equipped with smart TV, mini-fridge, desk, chairs. Enjoy City views at the sitting area by the window. Located on the 3rd floor with no elevator.",
     address: "1029  Godfrey Street, Portland, OR, 97205",
     guestCapacity: 2,
-    numOfBeds: 2,
+    numOfBeds: 5,
     isInternet: true,
     isBreakfast: true,
     isAirConditioned: true,
